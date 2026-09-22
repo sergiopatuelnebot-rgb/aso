1ea920bb1fe95efde885322d448d11a3bbf00bbb
Sergio_Patuel <sergiopatuelnebot@gmail.com>
2026-09-22 12:29:16 +0200

    he corregido algunos bash que no estaban del todo bien

--- Bash/Sesion1_repaso/Bloque_C/resumen_logs.sh
+++ Bash/Sesion1_repaso/Bloque_C/resumen_logs.sh
@@ -1,4 +1,8 @@
-ERROR=$(grep -c "ERROR" ~/prueba_bash/datos/app*)
-WARNING=$(grep -c "WARNING" ~/prueba_bash/datos/app*)
-echo "Número de errores: $ERROR"
-echo "Número de advertencias: $WARNING"
+#!/bin/bash
+carpeta=$1
+for fichero in "$carpeta"/*.log; do
+    nombre=$(basename "$fichero")
+    warnings=$(grep -c "WARNING" "$fichero")
+    errores=$(grep -c "ERROR" "$fichero")
+    echo "$nombre -> $warnings WARNING, $errores ERROR"
+done
