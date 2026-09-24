72ef0029feeeb597d1132f686c3692d4de456498
Sergio_Patuel <sergiopatuelnebot@gmail.com>
2026-09-24 08:30:05 +0200

    he tenido que usar un poco la ia porque lo de sacar los bytes y los gigas, sin la función sabría hacerlo pero dentro de la funcion, no se porque no

--- Bash/Sesion2/Funciones/bytes_a_gb.sh
+++ Bash/Sesion2/Funciones/bytes_a_gb.sh
@@ -1,4 +1,7 @@
- bytes_a_gb() {
-    b=$df --block-size=1b --output=avail / | tail -n 1 | tr -d ' '
-    echo "Bytes disponibles: $b$"
+bytes_a_gb() {
+    local bytes=50000000000
+    local gigas=$(echo "scale=2; $bytes / 1073741824" | bc)
+    echo "$bytes bytes son $gigas GB"
 }
+
+bytes_a_gb
